7de7862f0a55b0bfa4297a60c8c0eba5e9528c3e
Mike <102953584+michelangelodorado@users.noreply.github.com>
2023-02-09 17:58:36 +0800

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -33,8 +33,8 @@ sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 su - admin -c 'python3.11 -m pip install --user ansible-core==2.14.0'
 wait
 cd /home/admin/
-mkdir -p opt/ansible/files opt/ansible/collections opt/ansible/playbooks opt/ansible/roles opt/ansible/scripts opt/ansible/templates opt/ansible/inventory/groupvars opt/ansible/inventory/hostvars
-cd /home/admin/opt/ansible/
+mkdir -p ansible/files ansible/collections ansible/playbooks ansible/roles ansible/scripts ansible/templates ansible/inventory/groupvars ansible/inventory/hostvars
+cd /home/admin/ansible/
 cat << EOF | sudo tee ansible.cfg
 [defaults]
 inventory = ./inventory
